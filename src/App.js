53438067e724f181806c8c0ed251f7a8d70fde73
Stacey Teltser <stacey.teltser@gmail.com>
2021-09-12 16:33:54 -0500

    added form to page but no functionality yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Weekend Demo App</h1>
+        <Form>
+          <Form.Label>Search for a City</Form.Label>
+          <Form.Control type="city" placeholder="Enter city" />
+        </Form>
       </div>
     );
   }
