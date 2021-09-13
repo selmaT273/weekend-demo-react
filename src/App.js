3029499115cf70f3ceb7101019f39d8f451b4507
Stacey Teltser <stacey.teltser@gmail.com>
2021-09-12 21:09:26 -0500

    refactored to use bootstrap search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
+const locationKey = process.env.REACT_APP_LOCATION_KEY;
 export default class App extends React.Component {
   constructor(props){
     super(props);
@@ -9,24 +13,38 @@ export default class App extends React.Component {
     }
   }
   
-  handleLocationSearch = event => {
+  handleLocationSearch = async event => {
     event.preventDefault();
 
     let input= event.target.elements.citySearch.value;
-    
-    console.log(input);
+
+    this.setState({
+      searchedCity: input,
+    });
+
+    const locationUrl = `https://us1.locationiq.com/v1/search.php`;
+
+    const locationResponse = await axios.get(locationUrl, {
+      params: {
+        key: locationKey,
+        q: input,
+        format: 'json',
+      }
+    });
+
+    console.log(locationResponse.data[0]);
   }
 
   render() {
     return (
       <div>
         <h1>Weekend Demo App</h1>
-        <form onSubmit={this.handleLocationSearch}>
+        <Form onSubmit={this.handleLocationSearch}>
           <label>Search for a City
             <input type="text" name="citySearch" placeholder="Enter city" />
           </label>
-          <button type="submit">Search</button>
-        </form>
+          <Button type="submit" value="submit">Search</Button>
+        </Form>
       </div>
     );
   }
